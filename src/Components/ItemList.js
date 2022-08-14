01f5244ae82b293aae6ab09030141cd1ebec3109
Mariano Ponce <74881484+Over19@users.noreply.github.com>
2022-08-14 20:29:08 -0300

    arreglos

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,16 +1,4 @@
-import { useState, useEffect } from "react";
-import PacmanLoader from "react-spinners/PacmanLoader";
 import Item from "./Item";
-
-const styles = {
-	container: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "space-around",
-		alignItems: "center",
-		flexWrap: "wrap",
-	},
-};
 
 const ItemList = ({ productos }) => {
 	return (
